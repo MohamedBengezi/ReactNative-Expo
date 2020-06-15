1781aad521f52e8297520f53b0c706e5ee8f0ad2
Mohamed Bengezi <bengezim@mcmaster.ca>
2020-06-15 15:01:34 -0400

    Changing counter to use useReducer rather than setState

--- src/screens/counterScreen.js
+++ src/screens/counterScreen.js
@@ -6,18 +6,16 @@ const INCREMENT = 1
 
 const reducer = (state, action) => {
     //state object and the change to make to it. 
-    //state == {red: 0 ,......}. action == change_red || change_blue || change_green : colour, payload: amt
+    //state == {counter: 0}. action == increase || change_blue || change_green : colour, payload: amt
     switch (action.type) {
-        case 'increase_counter':
+        case 'increase':
             return { ...state, counter: state.counter + INCREMENT };
-        case 'decrease_counter':
+        case 'decrease':
             return { ...state, counter: state.counter - INCREMENT };
         default:
             return state;
     }
 }
-
-
 
 const CounterScreen = () => {
     const [state, dispatch] = useReducer(reducer, { counter: 0 });
@@ -25,8 +23,8 @@ const CounterScreen = () => {
 
     return (
         <View>
-            <Button title="Increase" onPress={() => { dispatch({ type: 'increase_counter', payload: INCREMENT }) }} />
-            <Button title="Decrease" onPress={() => { dispatch({ type: 'decrease_counter', payload: INCREMENT }) }} />
+            <Button title="Increase" onPress={() => { dispatch({ type: 'increase', payload: INCREMENT }) }} />
+            <Button title="Decrease" onPress={() => { dispatch({ type: 'decrease', payload: INCREMENT }) }} />
             <Text>Current Count: {counter}</Text>
         </View>
     );
